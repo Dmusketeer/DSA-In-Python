3bd1ee1a91c9be485985bbbb919eaec614ee3fd6
Dmuskeeter <tdheeraj9a@gmail.com>
2022-09-30 21:42:09 +0530

    matrix

---
+++ must-do-DSA/DAY_4/matrix.py
@@ -0,0 +1,10 @@
+# Matrix: A matrix represents a collection of numbers arranged in an order of rows and columns.
+#  It is necessary to enclose the elements of a matrix in parentheses or brackets.
+
+
+# Subarray: A subarray is a contiguous part of an array, i.e., a Subarray is an array that is inside another array.
+# In general, for an array of size n, there are n*(n+1)/2 non-empty subarrays.
+
+
+# Subset: If a Set has all its elements belonging to other sets, this set will be known as a subset of the other set.
+# A Subset is denoted as “⊆“. If set A is a subset of set B, it is represented as A ⊆ B.
