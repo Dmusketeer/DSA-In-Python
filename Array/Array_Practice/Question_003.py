89a20e9861ff745301fdd2498166c6951ec27430
Dmuskeeter <tdheeraj9a@gmail.com>
2023-03-15 16:16:51 +0530

    removing duplicate elements from array

--- Array/Array_Practice/Question_003.py
+++ Array/Array_Practice/Question_003.py
@@ -1,1 +1,14 @@
-# Check if array is sorted or not
+# remove the diplicate elements from the array
+def remove_duplicates(my_list):
+    lst = []
+    for i in my_list:
+        if i not in lst:
+            lst.append(i)
+    my_list = lst
+    return my_list
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+    print("The list with unique elements only:")
+    print(remove_duplicates(my_list))
