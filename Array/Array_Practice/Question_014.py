346858df73f28db4144492da58e0d793590f6300
Dmuskeeter <tdheeraj9a@gmail.com>
2023-03-27 16:39:30 +0530

    second largest element in array

--- Array/Array_Practice/Question_014.py
+++ Array/Array_Practice/Question_014.py
@@ -9,12 +9,26 @@
 # Find Second largest element in an array
 
 # Naive approach:
+# def second_largest_element(arr):
+#     if len(arr) < 2:
+#         print("Invalid input")
+#         return
+#     arr.sort()
+#     return f"Second largest element is : {arr[-2]}"
+
+
 def second_largest_element(arr):
-    if len(arr) < 2:
-        print("Invalid input")
-        return
-    arr.sort()
-    return f"Second largest element is : {arr[-2]}"
+    f = s = min(arr)
+    for i in range(len(arr)):
+        if arr[i] > f:
+            s = f
+            f = arr[i]
+        elif arr[i] < f and arr[i] > s:
+            s = arr[i]
+    return f"Second largest element is : {s}"
+
+
+# Time Complexity: O(n).
 
 if __name__ == "__main__":
     arr = [70, 11, 20, 4, 100]
